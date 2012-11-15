588804087aa3f0c758914dfcc26fb40cc0bae73c
chetrebman <chet2003@cybox.com>
2012-11-15 10:32:47 -0700

    first commit

--- batchScripts/shipIt.sh
+++ batchScripts/shipIt.sh
@@ -2,12 +2,17 @@
 
 echo
 echo "This script is used internally to get the distrubution ready for release"
+echo
+echo "THIS WILL OVERWRITE applicationContext.xml!!!!!"
+echo
+echo "   Copying  documentation from the javadev/fedoraApp/doc/* to ../doc" 
+cp -r ~/javadev/fedoraApp/doc/* ../doc/
 
-echo "   Coping  TEMPLATES/applicationContext.xml TO ../jetty/webapps/fedoraProxy/WEB-INF/applicationContext.xml"
+echo "   Copying  TEMPLATES/applicationContext.xml TO ../jetty/webapps/fedoraProxy/WEB-INF/applicationContext.xml"
 
 cp TEMPLATES/applicationContext.xml ../jetty/webapps/fedoraProxy/WEB-INF/applicationContext.xml
 
-echo "   Coping  TEMPLATES/webSiteCollection.properites TO ../jetty/webapps/fedoraProxy/WEB-INF/webSiteCollection.properties"
+echo "   Copying  TEMPLATES/webSiteCollection.properites TO ../jetty/webapps/fedoraProxy/WEB-INF/webSiteCollection.properties"
 
 cp TEMPLATES/webSiteCollection.properties ../jetty/webapps/fedoraProxy/WEB-INF/config/webSiteCollection.properties
 
